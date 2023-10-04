59b97f11c33b1d99caec9b0d254ef8c032d67a25
Rashmi Kamath <rush@frontm.com>
2023-10-04 17:24:34 +0530

    bug fix

--- edge/config.js
+++ edge/config.js
@@ -9,17 +9,18 @@ const DOCKER_CONFIG = {
   MONGO_URI: "mongodb://mongo:27017/olg",
   REDIS_HOST: "redis",
 };
+const ADMIN_ROLE = 'admin';
 const config = {
   REDIS_PORT: 6379,
   EXPRESS_PORT: 4001,
   ASSET_ROOT_PATH: __dirname,
   ASSETS_LOCATION: './bots',
-  ADMIN_ROLE: 'admin',
-  ADMIN_USER: { "userName": "Admin", "userId": "admin", "domains": [{"domain": "olg", "roles": ["enduser"]}], "dateOfBirth": 949464502000, pin: '7531', "userRole": this.ADMIN_ROLE},
+  ADMIN_USER: { "userName": "Admin", "userId": "admin", "domains": [{"domain": "olg", "roles": ["enduser"]}], "dateOfBirth": 949464502000, pin: '7531', "userRole": ADMIN_ROLE},
   IMO_KEY: 'EDGE_IMO',
   LAST_SYNC_TIME_KEY: 'LAST_SYNC_TIME',
   NODE_ID_KEY: 'NODE_ID',
 }
+config.ADMIN_ROLE = ADMIN_ROLE;
 
 if(env === 'docker') {
   Object.assign(config, DOCKER_CONFIG);
