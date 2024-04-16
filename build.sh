191b287ecaee007f0abb2b779250f9663aa9a5a3
soumyanaveen1996 <120354069+soumyanaveen1996@users.noreply.github.com>
2024-04-16 17:58:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -35,36 +35,6 @@ docker save edge webapp mongo-frontm redis-frontm -o ./out/out.tar
 
 zip -r $OUT ./out
 
-echo "Build generated $OUT" in
-    dev_frontm)
-        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_frontm --build-arg DOMAIN=${input_builddomain_type}"
-        ;;
-    dev_olg)
-        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_olg --build-arg DOMAIN=${input_builddomain_type}"
-        ;;
-    prod_frontm)
-        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_frontm --build-arg DOMAIN=${input_builddomain_type}"
-        ;;
-    prod_olg)
-        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_olg --build-arg DOMAIN=${input_builddomain_type}"
-        ;;
-    *)
-        echo "Unknown build type: ${input_build_type} or domain type: ${input_builddomain_type}"
-        exit 1
-        ;;
-esac
-
-echo "Building images"
-echo "Using build args: ${build_args}"
-docker-compose build --no-cache ${build_args}
-docker-compose pull mongo redis
-
-echo "Exporting images"
-docker image tag mongo:4.4.6 mongo-frontm
-docker image tag redis:alpine redis-frontm
-docker save edge webapp mongo-frontm redis-frontm -o ./out/out.tar
-
-zip -r $OUT ./out
-
-echo "Build generated $OUT"
+echo "Build generated $OUT" 
+ 
 ~                   
