dde4cd7480b154a31ab7ae9b89023a4e61a1bec8
soumyanaveen1996 <120354069+soumyanaveen1996@users.noreply.github.com>
2024-04-16 13:15:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,39 +3,43 @@
 OUT="out.zip"
 
 DEFAULT_BUILD_TYPE="dev_frontm"
-
+DEFAULT_BUILDDOMAIN_TYPE="OLG"
 input_build_type=${1:-$DEFAULT_BUILD_TYPE}
 echo "input_build_type = ${input_build_type}"
+input_builddomain_type=${1:-$DEFAULT_BUILDDOMAIN_TYPE}
+echo "input_builddomain_type = ${input_builddomain_type}"
 
 case $input_build_type in
     dev_frontm)
-        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_frontm"
+        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_frontm --build-arg DOMAIN=$input_builddomain_type"
         ;;
     dev_olg)
-        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_olg"
+        build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_olg --build-arg DOMAIN=$input_builddomain_type"
         ;;
     prod_frontm)
-        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_frontm"
+        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_frontm --build-arg DOMAIN=$input_builddomain_type"
         ;;
     prod_olg)
-        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_olg"
+        build_args="--build-arg EDGE_BUILD_TYPE=prod --build-arg WEBAPP_BUILD_TYPE=docker_olg --build-arg DOMAIN=$input_builddomain_type"
         ;;
     *)
         echo "Unknown build type: $input_build_type"
+        echo "Unkonwn builddomain type: $input_builddomain_type"
         exit 1
         ;;
 esac
 
 echo "Building images"
 echo "Using build args: $build_args"
-docker compose build $build_args
+docker compose build --no-cache $build_args
 docker compose pull mongo redis
 
 echo "Exporting images"
-docker image tag mongo mongo-frontm
+docker image tag mongo:4.4.6 mongo-frontm
 docker image tag redis:alpine redis-frontm
 docker save edge webapp mongo-frontm redis-frontm -o ./out/out.tar
 
 zip -r $OUT ./out
 
 echo "Build generated $OUT"
+~                            
