94a9bceeef8ebede1cef45f994d62843c8a0afb5
soumyanaveen1996 <120354069+soumyanaveen1996@users.noreply.github.com>
2024-04-16 17:26:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-DEFAULT_BUILD_TYPE="dev_frontm"
 
-
-input_build_type=${1:-$DEFAULT_BUILD_TYPE}
-input_builddomain_type=${2:-$DEFAULT_BUILDDOMAIN_TYPE}
 
 echo "input_build_type = ${input_build_type}"
 echo "input_builddomain_type = ${input_builddomain_type}"
 
-case ${input_build_type} in
+case ${input_build_type} && ${input_builddomain_type} in
     dev_frontm)
         build_args="--build-arg EDGE_BUILD_TYPE=dev --build-arg WEBAPP_BUILD_TYPE=docker_frontm --build-arg DOMAIN=${input_builddomain_type}"
         ;;
